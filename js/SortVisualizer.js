8d45b56cd96e796e7e56dc18d7b110ca0f77ba46
Arnoud <varnoud67@gmail.com>
2025-05-14 08:26:07 +0200

    Making graph with a bit of styling

---
+++ js/SortVisualizer.js
@@ -0,0 +1,47 @@
+const graphContainer = document.getElementById("graph");
+const stepCounter = document.getElementById("stepCounter");
+
+const arraysAreEqual = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+};
+
+const bubbleSortSteps = function(arr) {
+    let steps = [[...arr]];
+    let n = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+                if (!arraysAreEqual(arr, steps[steps.length - 1]))
+                    steps.push([...arr]);
+            }
+        }
+        n--;
+    } while (swapped);
+    return steps;
+};
+
+const originalNumbers = [3, 9, 4, 1, 2, 8, 3, 5];
+let steps = bubbleSortSteps([...originalNumbers]);
+
+function renderStep(stepIndex) {
+    const values = steps[stepIndex];
+    graphContainer.innerHTML = '';
+    values.forEach(num => {
+        const bar = document.createElement("div");
+        bar.className = "bar";
+        bar.style.height = `${(num / 10) * 100}%`;
+        bar.title = num;
+        graphContainer.appendChild(bar);
+    });
+    stepCounter.innerText = `Step: ${stepIndex + 1} / ${steps.length}`;
+}
+
+renderStep(0);
